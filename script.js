e750fd49811823b582efbec019da4d9c82df7f55
Justinas Bei <justinasbeinorius@protonmail.ch>
2025-05-15 06:46:08 +0000

    Add functionality to set custom product parameters

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('DEMO', document, window)
+
+const customProductParams = document.querySelector('.product-custom-text-container textarea[aria-label^="Kita"]')
+
+if (customProductParams) {
+    customProductParams.value = 'Test 123'
+    customProductParams.dispatchEvent(new Event('input', { bubbles: true }))
+}
